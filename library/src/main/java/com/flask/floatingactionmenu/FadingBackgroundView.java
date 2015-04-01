4011f5a1c2c9d23486f80eac79837e6b2c787883
flask <QuadFlask>
2015-04-01 18:17:05 +0900

    add fading color attr

--- library/src/main/java/com/flask/floatingactionmenu/FadingBackgroundView.java
+++ library/src/main/java/com/flask/floatingactionmenu/FadingBackgroundView.java
@@ -1,6 +1,7 @@
 package com.flask.floatingactionmenu;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -14,10 +15,23 @@ public class FadingBackgroundView extends View {
 
 	public FadingBackgroundView(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		init(context, attrs);
 	}
 
 	public FadingBackgroundView(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
+		init(context, attrs);
+	}
+
+	private void init(Context context, AttributeSet attrs) {
+		TypedArray attr = getTypedArray(context, attrs, R.styleable.FloatingActionButton);
+		if (attr != null) {
+			try {
+				color = attr.getColor(R.styleable.FloatingActionButton_fab_fadingColor, 0x80ffffff);
+			} finally {
+				attr.recycle();
+			}
+		}
 	}
 
 	@Override
@@ -47,5 +61,9 @@ public class FadingBackgroundView extends View {
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		return getAlpha() != 0 & super.onTouchEvent(event);
+	}
+
+	protected TypedArray getTypedArray(Context context, AttributeSet attributeSet, int[] attr) {
+		return context.obtainStyledAttributes(attributeSet, attr, 0, 0);
 	}
 }
